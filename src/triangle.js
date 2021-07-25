a9013c9ac33350520681cfc5bb62793d2fb6cdfe
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-07-25 10:30:03 -0700

    second passing test

--- src/triangle.js
+++ src/triangle.js
@@ -1,9 +1,13 @@
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
 }
-
+test('should correctly determine whether three lengths are not a triangle', () => {
+ const notTriangle = new Triangle(3,9,22);
+  expect(notTriangle.checkType()).toEqual("not a triangle");
+});
 Triangle.prototype.checkType = function() {
-  return "I can't answer that yet!";
-}
+  return "not a triangle";
+};
+
